2c85108afe4b04b5ae38a338989b77361be9f5ef
yudwcom <yudwcom@163.com>
2017-11-19 16:21:34 +0800

    跳转专用

---
+++ codetoany/getcode.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <title>该链接已经失效</title>
+    <link rel="stylesheet" href="http://www.yykddn.com/public/css/weui.min.css"/>
+<style type="text/css">
+body{background-color:#efeff4;-webkit-tap-highlight-color:transparent;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none
+}
+
+.container {
+padding-bottom: 20px
+}
+
+.weui-cells_checkbox>.weui-cell>* {
+pointer-events: none
+}
+
+.list__textarea-label_1IUds {
+float: left;
+color: #222;
+margin-bottom: 10px
+}
+
+.list__no-arrow_1QzDq:after {
+display: none!important
+}
+
+.list__warn_qnkn9 {
+color: #e64340!important
+}
+
+a.weui-cell {
+color: #222
+}
+
+.weui-cell__ft {
+font-size: 0
+}
+
+.zh_CN .weui-cell__ft,.zh_HK .weui-cell__ft,.zh_TW .weui-cell__ft {
+font-size: 17px}
+
+.comment__textarea-label_3n9Pv{float:left;color:#222;margin-bottom:10px}
+
+</style>
+</head>
+<body>
+    <div id="container" class="container"><div class="zh_CN"><div class="weui-msg">
+    <div class="weui-msg__icon-area">
+        <i class="weui-icon-warn weui-icon_msg"></i>
+    </div>
+    <div class="weui-msg__text-area">
+        <h2 class="weui-msg__title">该链接已经失效</h2>
+        <p class="weui-msg__desc">请从【驿源】公众号进入</p>
+    </div>
+    <div class="weui-msg__icon-area">
+        <img src="http://www.yykddn.com/public/job/qrcode_gh.jpg" width="60%">
+    </div>
+</div></div></div>
+
+</body></html>
