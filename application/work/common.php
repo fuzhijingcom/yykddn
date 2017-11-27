5aabd5bac2a2d6c5e39d5d4b9b22a8efcb85915b
yudwcom <yudwcom@163.com>
2017-11-28 02:16:16 +0800

    删除无用

--- application/work/common.php
+++ application/work/common.php
@@ -72,13 +72,9 @@ function get_name_by_type($type) {
     return $kuaidi_name;
 }
 function get_name_by_uid($uid) {
-    $name = M('yuangong') ->where('yid',$uid)->getField('name');
+   
+    $name = M('users_qiang') ->where('user_id',$uid)->getField('name');
     
-    if(!$name){
-        $name = M('users_qiang') ->where('user_id',$uid)->getField('name');
-    }else{
-        return $name;
-    }
     
     if(!$name){
         $name = M('user_address') ->where('user_id',$uid)->getField('consignee');
